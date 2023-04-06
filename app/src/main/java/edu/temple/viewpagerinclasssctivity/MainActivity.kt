15f244a79b6f50b3ec403980258359093b782bb8
Jason Michel <tuk94727@temple.edu>
2023-04-06 15:18:05 -0400

    Completed assignment so add button adds new fragment and jumps to that fragment. Also added remove button but doesn't navigate

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,26 +2,45 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
+    private val newButton: Button by lazy {
+        findViewById(R.id.button2)
+    }
+    private val removeButton: Button by lazy {
+        findViewById(R.id.button)
+    }
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPagerDisplay)
     }
+
+    private var numberOfPages = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewPager.adapter = object: FragmentStateAdapter(this) {
-            override fun getItemCount() = 10
-
-            override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
-
+        newButton.setOnClickListener{
+            numberOfPages++
+            viewPager.adapter?.notifyItemInserted(numberOfPages - 1)
+            viewPager.setCurrentItem(numberOfPages - 1, true)
+            //supportFragmentManager.findFragmentByTag("f" + position)
+            // How you access an actual fragment itself, can be used to find title of page
+        }
+        removeButton.setOnClickListener{
+            numberOfPages--
+            viewPager.adapter?.notifyItemRemoved(numberOfPages - 1)
         }
 
-    }
 
+        viewPager.adapter = object: FragmentStateAdapter(this) {
+            override fun getItemCount() = numberOfPages
+
+            override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
+        }
+    }
 }
